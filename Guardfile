2d17f8e702248ecca7f83bcb77ae9ba07a534879
Igor Hlina <srigi@srigi.sk>
2012-05-17 14:43:31 +0200

    SASS introduction. LiveReload as well.

---
+++ Guardfile
@@ -0,0 +1,12 @@
+## ######################## ##
+##           SASS           ##
+## ######################## ##
+
+guard 'compass', :configuration_file => 'sass.config.rb' do
+	watch(%r{^sass/.+\.s[ac]ss})
+end
+
+guard 'livereload' do
+  watch(%r{css/.+\.css})
+  watch(%r{images/.+\.(gif|jpg|png)})
+end
---
+++ sass.config.rb
@@ -0,0 +1,14 @@
+environment				= :development
+preferred_syntax		= :sass			# sass-convert -R --from scss --to sass sass scss
+output_style			= :nested		# :expanded, :nested, :compact, :compressed
+line_comments			= false
+relative_assets			= false
+
+# filesystem paths
+sass_dir				= "sass"
+css_dir					= "css"
+images_dir				= "images"
+
+# web paths
+http_stylesheets_path	= "/css"
+http_images_path		= "/images"
